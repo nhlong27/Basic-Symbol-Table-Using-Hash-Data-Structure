9a0e14c642c4f19131e1924e6de75f36c18fbf7a
Nguyen Hoang Long <long.nguyen2706@hcmut.edu.vn>
2021-12-04 23:48:03 +0700

    added condition to check instruction's validity
    file getline and regex check instructions

--- SymbolTable.cpp
+++ SymbolTable.cpp
@@ -2,5 +2,25 @@
 
 void SymbolTable::run(string filename)
 {
-    cout << "success";
+    fstream my_file;
+    my_file.open(filename, ios::in);
+    if (!my_file) cout<<"No such file.";
+
+    regex regex_settings("(LINEAR|QUADRATIC|DOUBLE|INSERT|ASSIGN|BEGIN|LOOKUP|END|PRINT)(\\s[0-9]{1,6})(\\s[0-9]{1,6})");
+    string settings;
+    getline(my_file, settings);
+        if (!regex_match(settings, regex_settings)){
+            throw InvalidInstruction(settings);
+    }
+    regex regex_instruction("(INSERT|ASSIGN|BEGIN|LOOKUP|END|PRINT)(\\s([0-9]+|\'[a-z0-9\\s]+\'|[a-z][a-zA-Z_0-9]*|[a-z][a-zA-Z_0-9]*\\(([0-9]+|\'[a-z0-9\\s]+\'|[a-z][a-zA-Z_0-9]*|\\,)+\\)))?(\\s([0-9]+|\'[a-z0-9\\s]+\'|[a-z][a-zA-Z_0-9]*|[a-z][a-zA-Z_0-9]*\\(([0-9]+|\'[a-z0-9\\s]+\'|[a-z][a-zA-Z_0-9]*|\\,)+\\)))?");
+    string instruction;
+    while(getline(my_file, instruction)){
+        if (!regex_match(instruction, regex_instruction)){
+            throw InvalidInstruction(instruction);
+        }
+        
+
+    }
+    cout<< "success";
+
 }
